727e1aae48b9efe305ff5e904ab3d93b0d9eec5d
Greg Ross <gregandrewross79@gmail.com>
2023-10-07 14:50:20 +0100

    First Commit

---
+++ Encapsulate/EncapsulateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EncapsulateApp.swift
+//  Encapsulate
+//
+//  Created by Greg Ross on 07/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct EncapsulateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
